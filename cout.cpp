ab65d8335d1509023d6023c7e8a61b86d44059bd
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2026-01-27 21:40:37 -0500

    Roadmap.sh learning basics of C++ but adding the "using namespace std;"

--- cout.cpp
+++ cout.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b) {
+    return a + b;
+}
+
+class Calculator {
+    public:
+        int multiply(int a, int b) {
+            return a * b;
+        }
+};
+
 int main() {
-    cout << "Hello World!" << endl;
+    int x = 5;
+    int y = 3;
+
+    int sum = add(x, y);
+    cout << "Sum: " << sum << '\n';
+
+    Calculator calc;
+    int product = calc.multiply(x, y);
+    cout << "Product: " << product << '\n';
+
     return 0;
 }
